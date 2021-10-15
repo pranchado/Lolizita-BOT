81305ae48567b66fea794f21a3ceea784259def2
ItaluH <77984589+italuH@users.noreply.github.com>
2021-10-15 09:52:59 -0300

    Add files via upload

---
+++ database/help/help.js
@@ -0,0 +1,56 @@
+const help = (p, hr, pushname, prema, checATM, useLevel, useXp, requireXp, patt, bateria, chatss, antilink_by_tobi, welcome_by_tobi, levelingon_by_tobi, antifaker_by_tobi, selfchat_by_tobi, tempo) => {
+ return `
+ ${tempo}
+╭═══════════════⊷
+╰╮ ⟨❗⟩️ 𝖨𝗇𝖿𝗈 𝖽𝗈 𝖡𝗈𝗍 ⟨❗⟩
+╭┤⋟ Pʀᴇғɪx:『${p}』
+┃│⋟ Hᴏʀᴀ: ${hr}
+┃│⋟ Tᴏᴛᴀʟ ᴄʜᴀᴛ: ${chatss}
+┃│⋟ Bᴀᴛᴇʀɪᴀ: ${bateria}
+┃╰═════════════⊷
+╰╦═════「★」═════⊷
+╭┤⟨👤⟩ 𝖲𝖾𝗎 𝖯𝖾𝗋𝖿𝗂𝗅 ⟨👤⟩
+┃╞═════════════⊷
+┃│⋟ Nᴏᴍᴇ: ${pushname}
+┃│⋟ Tɪᴘᴏ ᴅᴇ ᴜsᴜᴀʀɪᴏ: ${prema}
+┃│⋟ Dɪɴʜᴇɪʀᴏ: ${checATM}
+┃│⋟ Lᴇᴠᴇʟ: ${useLevel}
+┃│⋟ Xᴘ: ${useXp}/${requireXp}
+┃│⋟ Pᴀᴛᴇɴᴛᴇ: ${patt}                        
+┃╰═════════════⊷
+╰╦═════「★」═════⊷
+╭┤⟨👾⟩ 𝖭𝗈𝗏𝗈𝗌 𝖬𝖾𝗇𝗎 ⟨👾⟩
+┃╞═════════════⊷
+┃│⋟ ${p}Grupo
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖠𝖽𝗆𝗌 ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Figurinhas
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖲ᴛɪᴄᴋᴇʀs ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Nuvem
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖭ᴜᴠᴇɴs ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Animes
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖠ɴɪᴍᴇs ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Playrs
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖠ᴜᴅɪᴏs ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Jogos
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖩ᴏɢᴏs ⟨⟩
+┃╰━━━─── • ──━━━━
+┃╭ ⋟ ${p}Logos
+┃│⟨⟩ 𐌼ᴇɴᴜ ժҽ 𝖫ᴏɢᴏs ⟨⟩
+┃╰━━━─── • ──━━━━
+╰┬═════════════⊷
+╭┤️⟨⟩ 𝙸ɴғᴏʀᴍᴀᴄ̧ᴏ̃ᴇs ⟨⟩
+┃│
+┃│✙◗𝚂𝖾𝗅𝖿𝖼𝗁𝖺𝗍: ${selfchat_by_tobi}  
+┃│✙◗𝚆ᴇʟᴄᴏᴍᴇ: ${welcome_by_tobi} 
+┃│✙◗𝙻ᴇᴠᴇʟɪɴɢ: ${levelingon_by_tobi} 
+┃│✙◗𝙰ɴᴛɪғᴀᴋᴇ: ${antifaker_by_tobi} 
+┃│✙◗𝙰ɴᴛɪʟɪɴᴋ: ${antilink_by_tobi}
+┃╰═════════════⊷
+╰─━━━─── • ───━━━━❋ཻུ۪۪⸙`
+}
+exports.help = help
